aa67c94e8bc7dda41391856a58b2b41b0e946b69
Anthony Littlejohn <ant05man@yahoo.com>
2023-09-02 20:36:01 -0400

    finished index

--- index.js
+++ index.js
@@ -53,6 +53,11 @@ function writeToFile(fileName, data) {
     });
 }
 
+async function init() {
+    console.log("Starting init");
+	var svgString = "";
+	var svg_file = "logo.svg";
+
 const answers = inquirer.prompt(questions);
 
 var user_text ='';
@@ -78,3 +83,38 @@ console.log(`Your shape color is:${user_shape_color}`)
 user_shape_type = answer['choose-shape'];
 console.log(`Your shape chosen is:${user_shape_type}`)
 
+	//user shape else if
+	let user_shape;
+	if (user_shape_type === "Square" || user_shape_type === "square") {
+		user_shape = new Square();
+		console.log("User selected Square shape");
+	}
+	else if (user_shape_type === "Circle" || user_shape_type === "circle") {
+		user_shape = new Circle();
+		console.log("User selected Circle shape");
+	}
+	else if (user_shape_type === "Triangle" || user_shape_type === "triangle") {
+		user_shape = new Triangle();
+		console.log("User selected Triangle shape");
+	}
+	else {
+		console.log("Invalid shape!");
+	}
+	user_shape.setColor(user_shape_color);
+
+    	// Create a new Svg instance and add the shape and text elements to it
+	var svg = new Svg();
+	svg.setTextElement(user_text, user_font_color);
+	svg.setShapeElement(user_shape);
+	svgString = svg.render();
+	
+	//Print shape to log
+	console.log("Displaying shape:\n\n" + svgString);
+	//document.getElementById("svg_image").innerHTML = svgString;
+
+	console.log("Shape generation complete!");
+	console.log("Writing shape to file...");
+	writeToFile(svg_file, svgString); 
+}
+
+init()
